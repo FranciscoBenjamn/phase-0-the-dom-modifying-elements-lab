6a950c039bc8e6b752840bb815697a80b1d19438
Franklin Benjamin <franciscovibes14@gmail.com>
2022-03-25 19:19:00 -0400

    commit tree 1dd6b4f87871edf5ff0a0ffc86324561ba89a173

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector("#main");
+main.remove()
+
+var newHeader = document.createElement("h1"); 
+var t = newHeader.setAttribute("id","victory"); 
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+document.body.appendChild(newHeader); 
